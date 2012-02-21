74cbe21d117b730f2a29d48d29565d2a650ad4fd
Alexander Kirk Joergensen <alkii900@msn.com>
2012-02-21 15:14:47 +0100

    Added first layout of miningNobel

---
+++ miningnobel/MiningNobel.java
@@ -0,0 +1,27 @@
+public class MiningNobel 
+{
+    int count;
+    private static int N;
+    private static double[] A = new double[N];
+    
+    public void main(String[] args) {
+        for (int i = 0; i < N; ++i) {
+            A[i] = StdIn.readDouble();
+        }
+        this.count();
+    }
+    
+    private static int count() {
+        int count = 0;
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                for (int k = 0; k < N; ++k) {
+                    for (int l = 0; l < N; l++) {
+                        if (A[i] + A[j] + A[k] + A[l] == 0) { count++; }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}

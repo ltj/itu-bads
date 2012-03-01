128815544832d5b534b6fe7c90e91e3b7051f3d6
Lars Toft Jacobsen <lars@boxed.dk>
2012-03-01 10:43:11 +0100

    Begun Congress assignment

---
+++ congress/Congress.java
@@ -0,0 +1,8 @@
+public class Congress {
+	
+	public static void main(String[] args) {
+		PriorityQueue<Integer> q = new PriorityQueue<Integer>(100);
+		StdOut.println("Hello world");
+	}
+	
+}
---
+++ congress/State.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+
+public class State {
+	
+	private final String name;
+	private int seats;
+	private int cpop;
+	
+	public 
+	
+}

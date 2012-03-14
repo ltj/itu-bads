c297e58a5ce11c26f343cd34f4856bc8cb0708bb
Lars Toft Jacobsen <lars@boxed.dk>
2012-03-14 21:34:46 +0100

    all sorting methods implemented. only test framework missing.

--- quickinsertion/QuickInsertion.java
+++ quickinsertion/QuickInsertion.java
@@ -1,20 +1,13 @@
 public class QuickInsertion {
 	
-	// standard insertion sort - no external calls
+	// standard insertion sort
 	private static void iSort(int[] a) {
-		int N = a.length;
-		for (int i = 1; i < N; i++) {
-			for (int j = i; j > 0 && (a[j] < a[j-1]); j--) {
-				int t = a[j];
-				a[j] = a[j-1];
-				a[j-1] = t;
-			}
-		}
+		iSort(a, 0, a.length);
 	}
 	
 	// standard insertion sort of subarray - no external calls
 	private static void iSort(int[] a, int lo, int hi) {
-		for (int i = lo+1; i < hi; i++) {
+		for (int i = lo+1; i < hi+1; i++) {
 			for (int j = i; j > lo && (a[j] < a[j-1]); j--) {
 				int t = a[j];
 				a[j] = a[j-1];
@@ -23,43 +16,58 @@ public class QuickInsertion {
 		}
 	}
 	
-	// standard qsort - no external calls
+	// standard qsort
 	private static void qSort(int[] a, int lo, int hi) {
-		if (hi <= lo) return;
-		int p = partition(a, lo, hi);
-		qSort(a, lo, p-1);
-		qSort(a, p+1, hi);
+		if(lo < hi) {
+			int q = partition(a, lo, hi);
+			qSort(a, lo, q-1);
+			qSort(a, q+1, hi);
+		}
 	}
 	
+	// short qsort
 	private static void qSort(int[] a) {
 		qSort(a, 0, a.length-1);
 	}
 	
+	// quicksort w cutoff
+	private static void qSort(int[] a, int lo, int hi, int cutoff) {
+		if(hi-lo <= cutoff) {
+			iSort(a, lo, hi);
+		}
+		else if(lo < hi) {
+			int q = partition(a, lo, hi);
+			qSort(a, lo, q-1, cutoff);
+			qSort(a, q+1, hi, cutoff);
+		}
+	}
+	
+	// qsort partition
 	private static int partition(int[] a, int lo, int hi) {
-		int i = lo;
-		int j = hi + 1;
-	    int v = a[lo];
-		while (true) { 
-	    	while (a[++i] < v) if (i == hi) break;
-			while (v < a[--j])
-	        	if (j == lo) break;
-				if (i >= j) break;
-
-	    	int t = a[i];
-			a[i] = a[j];
-			a[j] = t;
+		int pivot = a[hi]; // set pivot to last element
+		int i = lo - 1; // index not yet known
+		
+		for (int j = lo; j < hi; j++) {
+			if (a[j] <= pivot) {
+				i++; // increase index if smaller part grows
+				// Swap
+				int t = a[i];
+				a[i] = a[j];
+				a[j] = t;
+			}
 		}
 
-		int t = a[lo];
-		a[lo] = a[j];
-		a[j] = t;
+		// Swap pivot
+		int t = a[i+1];
+		a[i+1] = a[hi];
+		a[hi] = t;
 
-	    return j;
+	    return i+1;
 	}
 	
 	// sort method - quicksort w insertion sort cutoff
 	public static void sort(int[] a, int cutoff) {
-		
+		qSort(a, 0, a.length-1, cutoff);
 	}
 	
 	// return randomized int list of size s
@@ -83,12 +91,13 @@ public class QuickInsertion {
 		
 		int[] test = randomIntList(10);
 		StdArrayIO.print(test);
-		iSort(test, 5, test.length);
+		iSort(test, 5, test.length-1);
 		StdArrayIO.print(test);
-		test = randomIntList(10);
+		test = randomIntList(1000);
 		StdArrayIO.print(test);
-		qSort(test);
+		sort(test, 10);
 		StdArrayIO.print(test);
+		
 	}
 	
 }

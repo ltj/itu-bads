8cfde593829d6011bdeb147bd0a0ee1de453cbdd
Alexander Kirk Joergensen <alkii900@msn.com>
2012-03-15 16:30:22 +0100

    QuickInsertionSort Readme udfyldt

--- RandomGenerator.java
+++ RandomGenerator.java
@@ -4,7 +4,7 @@ public class RandomGenerator {
 	private static int n;
 	
 	public static void main(String[] args) {
-		n = Integer.parseInt(args[0]);
+		n = Integer.parseInt(args[0])+1;
 		Out out = new Out("RandomNumbers"+n+".txt");
 		
 		for (int i = 0; i < n; i++) {
